d2d4a603317c457b313f1fa6941e05e8907bdc72
Rafal Labisz <rafal0710@gmail.com>
2019-06-04 11:06:51 +0200

    Updates

---
+++ precache-manifest.b68f278cc8844ed2ac3154674506273e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fdd7cbe7d9182d73e3fdf60017c3589",
+    "url": "/hackathon-aei/index.html"
+  },
+  {
+    "revision": "14a28f065201d962e48d",
+    "url": "/hackathon-aei/static/js/2.f7915d75.chunk.js"
+  },
+  {
+    "revision": "ed74237d6bc3ce241ea1",
+    "url": "/hackathon-aei/static/js/main.57621bc0.chunk.js"
+  },
+  {
+    "revision": "82ce4e865d5a4d9044d0",
+    "url": "/hackathon-aei/static/js/runtime~main.2e1128a9.js"
+  }
+]);
